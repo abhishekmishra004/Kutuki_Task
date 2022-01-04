dbfe561abbb87dd0513f0cb2df9db8402df8a4ac
Abhishek <abhishekmishragacr@gmail.com>
2022-01-04 20:25:13 +0530

    test class

---
+++ app/src/main/java/com/example/kutuki/TestActivity.kt
@@ -0,0 +1,12 @@
+package com.example.kutuki
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TestActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test)
+        //test activity for guideline of constraint layout
+    }
+}
